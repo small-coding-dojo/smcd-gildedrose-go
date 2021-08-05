731608c018e91e7231f0e16941f2090e9f034fec
Niklas Rueber <gglm1704@gmail.com>
2021-08-05 17:55:54 +0200

    test: added for normal item requirements. Co-authored-by: Tugbay Atilla <tatilla@igus.net> Co-authored-by: Stefan Boos <kontakt@boos.systems>

--- GildedRose/gilded-rose_test.go
+++ GildedRose/gilded-rose_test.go
@@ -6,14 +6,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Positive_SellIn_reduces_quality_by_one(t *testing.T) {
-	var items = []*Item{
-		&Item{"foo", 10, 100},
-	}
-	var expected = []*Item{
-		&Item{"foo", 9, 99},
-	}
-	UpdateQuality(items)
+func Test_all_specifications(t *testing.T) {
 
-	assert.Equal(t, expected, items)
+	testData := []struct {
+		desc     string
+		input    Item
+		expected Item
+	}{
+		{desc: "At the end of each day our system lowers both values for every item", input: Item{"foo", 10, 100}, expected: Item{"foo", 9, 99}},
+		{desc: "The Quality of an item is never negative", input: Item{"foo", 10, 0}, expected: Item{"foo", 9, 0}},
+		{desc: "Once the **sell by date** has passed, Quality degrades twice as fast", input: Item{"foo", -1, 10}, expected: Item{"foo", -2, 8}},
+		{desc: "Edge case: SellIn is 0 and Quality degrades twice as fast", input: Item{"foo", 0, 2}, expected: Item{"foo", -1, 0}},
+		{desc: "Edge case: Quality degrades twice as fast AND quality of an item is never negative", input: Item{"foo", -1, 1}, expected: Item{"foo", -2, 0}},
+	}
+
+	for _, record := range testData {
+		t.Run(record.desc, func(t *testing.T) {
+			items := []*Item{&record.input}
+			UpdateQuality(items)
+			assert.Equal(t, &record.expected, items[0])
+		})
+	}
 }
