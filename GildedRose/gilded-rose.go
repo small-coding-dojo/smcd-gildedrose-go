da81c44fc8184914ae1d91fcf5a5c902746840ac
Holger Janßen-Kroll <github@ho-jk.de>
2021-09-02 16:01:04 +0000

    refactor: unify quality changes
    Squashed commit of the following:
    
    commit 0a35ecf3c5733a6c54a902c3c71bc1a49555eef3
    Author: Holger Janßen-Kroll <github@ho-jk.de>
    Date:   Thu Sep 2 16:00:01 2021 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 3169669b50e460f787ad6dc438b816e4777afd62
    Author: Stefan Boos <kontakt@boos.systems>
    Date:   Thu Sep 2 17:50:54 2021 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 5ebe3d09c8e15daa194d0ccc94bfab475d9d571f
    Author: Falk Kühnel <falk@kuehnel.eu>
    Date:   Thu Sep 2 17:41:08 2021 +0200
    
        mob next [ci-skip]
    
    commit f90e84acaedd5408b5bac6a4f48534b4c769e601
    Author: Michael Mahlberg <mm@consulting-guild.de>
    Date:   Thu Sep 2 17:29:00 2021 +0200
    
        mob next [ci-skip]
    Co-authored-by: Falk Kühnel <falk@kuehnel.eu>
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- GildedRose/gilded-rose.go
+++ GildedRose/gilded-rose.go
@@ -16,7 +16,6 @@ func UpdateQuality(items []*Item) {
 
 		if currentItem.name == "Aged Brie" {
 			qualityChangesForAgedBrie(currentItem)
-			qualityChangesFroOverdueAgedBrie(currentItem)
 		} else if currentItem.name == "Backstage passes to a TAFKAL80ETC concert" {
 			qualityChangesForBackstagePasses(currentItem)
 		} else if currentItem.name == "Conjured Mana Cake" {
@@ -27,22 +26,8 @@ func UpdateQuality(items []*Item) {
 			qualityChangesForNormalItems(currentItem)
 		}
 
-		if currentItem.sellIn <= 0 {
-			if currentItem.name == "Aged Brie" {
-				// this is left blank intentionally, due to refactoring
-			} else if currentItem.name == "Backstage passes to a TAFKAL80ETC concert" {
-				currentItem.quality = 0
-			} else if currentItem.quality > 0 {
-				if currentItem.name == "Conjured Mana Cake" {
-					currentItem.quality -= 2
-				} else if currentItem.name != "Sulfuras, Hand of Ragnaros" {
-					currentItem.quality -= standardQualityChange
-				}
-			}
-		}
-
 		if currentItem.name != "Sulfuras, Hand of Ragnaros" {
-			currentItem.quality = capToMaxStandardQuality(currentItem.quality)
+			capToStandardMaximumQuality(currentItem)
 		}
 
 		// letTimePassBy
@@ -51,25 +36,24 @@ func UpdateQuality(items []*Item) {
 		} else {
 			agingForNonLegendaryItems(currentItem)
 		}
-
-
 	}
 
 }
 
-func agingForLegendaryItems(item *Item) {
-	
+func capToStandardMaximumQuality(item *Item) {
+	item.quality = int(math.Min(50, float64(item.quality)))
 }
 
-func agingForNonLegendaryItems(currentItem *Item) {
-	currentItem.sellIn -= 1
+func agingForLegendaryItems(item *Item) {
+
+}
+
+func agingForNonLegendaryItems(item *Item) {
+	item.sellIn -= 1
 }
 
 func qualityChangesForAgedBrie(item *Item) {
 	item.quality += standardQualityChange
-}
-
-func qualityChangesFroOverdueAgedBrie(item *Item) {
 	if item.sellIn <= 0 {
 		item.quality += standardQualityChange
 	}
@@ -79,10 +63,16 @@ func qualityChangesForNormalItems(item *Item) {
 	if item.quality > 0 {
 		item.quality -= standardQualityChange
 	}
+	if (item.sellIn <= 0) && (item.quality > 0) {
+		item.quality -= standardQualityChange
+	}
 }
 
 func qualityChangesForConjuredItems(item *Item) {
 	if item.quality > 0 {
+		item.quality -= 2
+	}
+	if (item.sellIn <= 0) && (item.quality > 0) {
 		item.quality -= 2
 	}
 }
@@ -95,12 +85,12 @@ func qualityChangesForBackstagePasses(item *Item) {
 	if item.sellIn <= 5 {
 		item.quality += standardQualityChange
 	}
+
+	if item.sellIn <= 0 {
+		item.quality = 0
+	}
 }
 
-func qualityChangesForLegendaryItems(item *Item){
+func qualityChangesForLegendaryItems(item *Item) {
 
-}
-
-func capToMaxStandardQuality(quality int) int {
-	return int(math.Min(50, float64(quality)))
 }
