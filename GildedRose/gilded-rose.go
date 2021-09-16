85f42c61cd3fa118e08264dc00b7119aecdd6d39
Falk Kühnel <falk@kuehnel.eu>
2021-09-16 19:00:24 +0200

    refactor: replacing factory with dictionary, and introducing chain of responsibility (partial)

--- GildedRose/gilded-rose.go
+++ GildedRose/gilded-rose.go
@@ -9,23 +9,37 @@ type Item struct {
 
 const standardQualityChange = 1
 
-type QualityStrategy interface {
-	ApplyQualityChanges(item *Item)
+func UpdateQuality(items []*Item) {
+	for i := 0; i < len(items); i++ {
+		currentItem := items[i]
+
+		qualityStrategy := qualityStrategyFor(currentItem)
+		qualityStrategy.ApplyChangesForOneDay(currentItem)
+	}
 }
 
-func qualityStrategyFor(item *Item) (strategy QualityStrategy) {
-	if isAgedBrie(item) {
-		strategy = AgedBrieQualityStrategy{}
-	} else if isBackstagePass(item) {
-		strategy = BackstagePassesQualityStrategy{}
-	} else if isConjuredItem(item) {
-		strategy = ConjuredItemQualityStrategy{}
-	} else if isLegendaryItem(item) {
-		strategy = LegendaryItemQualityStrategy{}
-	} else {
-		strategy = NormalItemQualityStrategy{}
+
+type QualityStrategy interface {
+	IsApplicableFor(item *Item) bool
+	ApplyChangesForOneDay(item *Item)
+}
+
+func qualityStrategyFor(item *Item) QualityStrategy {
+
+	strategies := []QualityStrategy{
+		AgedBrieQualityStrategy{},
+		BackstagePassesQualityStrategy{},
+		ConjuredItemQualityStrategy{},
+		LegendaryItemQualityStrategy{},
 	}
-	return
+
+	for _, qualityStrategy := range strategies {
+		if qualityStrategy.IsApplicableFor(item) {
+			return qualityStrategy
+		}
+	}
+
+	return NormalItemQualityStrategy{}
 }
 
 func isAgedBrie(item *Item) bool {
@@ -46,94 +60,26 @@ func isLegendaryItem(currentItem *Item) bool {
 
 type AgedBrieQualityStrategy struct{}
 
-func (a AgedBrieQualityStrategy) ApplyQualityChanges(item *Item) {
-	qualityChangesForAgedBrie(item)
+func (a AgedBrieQualityStrategy) IsApplicableFor(item *Item) bool {
+	return isAgedBrie(item)
 }
 
-type BackstagePassesQualityStrategy struct{}
-
-func (b BackstagePassesQualityStrategy) ApplyQualityChanges(item *Item) {
-	qualityChangesForBackstagePasses(item)
-}
-
-type ConjuredItemQualityStrategy struct {}
-
-func (c ConjuredItemQualityStrategy) ApplyQualityChanges(item *Item) {
-	qualityChangesForConjuredItems(item)
-}
-
-type LegendaryItemQualityStrategy struct {}
-
-func (l LegendaryItemQualityStrategy) ApplyQualityChanges(item *Item) {
-	qualityChangesForLegendaryItems(item)
-}
-
-type NormalItemQualityStrategy struct {}
-
-func (n NormalItemQualityStrategy) ApplyQualityChanges(item *Item) {
-	qualityChangesForNormalItems(item)
-}
-
-func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		currentItem := items[i]
-
-		qualityStrategy := qualityStrategyFor(currentItem)
-		qualityStrategy.ApplyQualityChanges(currentItem)
-
-		if !isLegendaryItem(currentItem) {
-			capToStandardMaximumQuality(currentItem)
-		}
-
-		// letTimePassBy
-		if isLegendaryItem(currentItem) {
-			agingForLegendaryItems(currentItem)
-		} else {
-			agingForNonLegendaryItems(currentItem)
-		}
-	}
-}
-
-func capToStandardMaximumQuality(item *Item) {
-	item.quality = int(math.Min(50, float64(item.quality)))
-}
-
-func agingForLegendaryItems(item *Item) {
-
-}
-
-func agingForNonLegendaryItems(item *Item) {
-	item.sellIn -= 1
-}
-
-func qualityChangesForAgedBrie(item *Item) {
+func (a AgedBrieQualityStrategy) ApplyChangesForOneDay(item *Item) {
 	item.quality += standardQualityChange
 	if item.sellIn <= 0 {
 		item.quality += standardQualityChange
 	}
+	capToStandardMaximumQuality(item)
+	applyStandardAging(item)
 }
 
-func qualityChangesForNormalItems(item *Item) {
-	if item.quality > 0 {
-		item.quality -= standardQualityChange
-	}
-	if (item.sellIn <= 0) && (item.quality > 0) {
-		item.quality -= standardQualityChange
-	}
+type BackstagePassesQualityStrategy struct{}
 
+func (b BackstagePassesQualityStrategy) IsApplicableFor(item *Item) bool {
+	return isBackstagePass(item)
 }
 
-func qualityChangesForConjuredItems(item *Item) {
-	if item.quality > 0 {
-		item.quality -= 2
-	}
-	if (item.sellIn <= 0) && (item.quality > 0) {
-		item.quality -= 2
-	}
-}
-
-func qualityChangesForBackstagePasses(item *Item) {
+func (b BackstagePassesQualityStrategy) ApplyChangesForOneDay(item *Item) {
 	item.quality += standardQualityChange
 	if item.sellIn <= 10 {
 		item.quality += standardQualityChange
@@ -145,8 +91,58 @@ func qualityChangesForBackstagePasses(item *Item) {
 	if item.sellIn <= 0 {
 		item.quality = 0
 	}
+	capToStandardMaximumQuality(item)
+	applyStandardAging(item)
 }
 
-func qualityChangesForLegendaryItems(item *Item) {
+type ConjuredItemQualityStrategy struct{}
 
+func (c ConjuredItemQualityStrategy) IsApplicableFor(item *Item) bool {
+	return isConjuredItem(item)
+}
+
+func (c ConjuredItemQualityStrategy) ApplyChangesForOneDay(item *Item) {
+	if item.quality > 0 {
+		item.quality -= 2
+	}
+	if (item.sellIn <= 0) && (item.quality > 0) {
+		item.quality -= 2
+	}
+	capToStandardMaximumQuality(item)
+	applyStandardAging(item)
+}
+
+type LegendaryItemQualityStrategy struct{}
+
+func (l LegendaryItemQualityStrategy) IsApplicableFor(item *Item) bool {
+	return isLegendaryItem(item)
+}
+
+func (l LegendaryItemQualityStrategy) ApplyChangesForOneDay(item *Item) {
+
+}
+
+type NormalItemQualityStrategy struct{}
+
+func (n NormalItemQualityStrategy) IsApplicableFor(item *Item) bool {
+	return true
+}
+
+func (n NormalItemQualityStrategy) ApplyChangesForOneDay(item *Item) {
+	if item.quality > 0 {
+		item.quality -= standardQualityChange
+	}
+	if (item.sellIn <= 0) && (item.quality > 0) {
+		item.quality -= standardQualityChange
+	}
+	capToStandardMaximumQuality(item)
+	applyStandardAging(item)
+}
+
+func capToStandardMaximumQuality(item *Item) {
+	item.quality = int(math.Min(50, float64(item.quality)))
+}
+
+func applyStandardAging(item *Item) {
+	item.sellIn -= 1
 }
